d70288d51f234ad94cc7c473385fb83473dc0bb1
Luiz Cláudio <luizclaudio@ufersa.edu.br>
2018-10-16 09:08:00 -0300

    feat: Add Loading Bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,12 +2,16 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveTweets } from './tweets';
 import { setAuthedUser } from '../actions/authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
-export const handleInitialData = () => dispatch =>
-  getInitialData().then(({ users, tweets }) => {
+export const handleInitialData = () => dispatch => {
+  dispatch(showLoading());
+  return getInitialData().then(({ users, tweets }) => {
     dispatch(receiveUsers(users));
     dispatch(receiveTweets(tweets));
     dispatch(setAuthedUser(AUTHED_ID));
+    dispatch(hideLoading());
   });
+};
