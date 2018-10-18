f2a9b6777353c033b166f726c0c0fed60f52f402
Luiz Claudio <luizcns18@gmail.com>
2018-10-18 07:05:20 -0300

    feat: Add routing

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -7,6 +7,7 @@ import {
   TiHeartFullOutline,
 } from 'react-icons/ti';
 import { handleToggleTweet } from '../actions/tweets';
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
   handleLike = e => {
@@ -25,7 +26,7 @@ class Tweet extends Component {
 
   toParent = (e, id) => {
     e.preventDefault();
-    // todo: Redirect to parent Tweet.
+    this.props.history.push(`/tweet/${id}`);
   };
 
   render() {
@@ -43,11 +44,12 @@ class Tweet extends Component {
       hasLiked,
       likes,
       replies,
+      id,
       parent,
     } = tweet;
 
     return (
-      <div className="tweet">
+      <Link to={`/tweet/${id}`} className="tweet">
         <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
         <div className="tweet-info">
           <div>
@@ -76,7 +78,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
@@ -93,4 +95,4 @@ const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
   };
 };
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
