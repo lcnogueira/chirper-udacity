920aac51441ba8401e0470719e9da849051b2c2b
Luiz Claudio <luizcns18@gmail.com>
2018-10-18 06:36:46 -0300

    feat: Create new tweet

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
   componentDidMount() {
@@ -14,8 +15,9 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {/* <NewTweet /> */}
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true ? null : (
+          <TweetPage match={{ params: { id: '8xf0y6ziyjabvozdd253nd' } }} />
+        )}
       </div>
     );
   }
